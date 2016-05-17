0b49cb79dc2238cf278f3142083fab212985c2c3
Al Olsen <alvaro.olsen@nyu.edu>
2016-05-16 20:45:30 -0400

    Added dynamic styles to clock

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 		// var ss = "00";
 		// var mm = "00";
 		// var hh = "00";
-
-		var dt = new Date();
-		var hh = dt.getHours();
-		var mm = dt.getMinutes();
-		var ss = dt.getSeconds();
+		var isOrange 		= false;
+		var dt 				= new Date();
+		var hh 				= dt.getHours();
+		var mm 				= dt.getMinutes();
+		var ss 				= dt.getSeconds();
 		function clock(){
 			//seTimeout( function, duration );
 			setTimeout(function(){
@@ -14,6 +14,20 @@ $(document).ready(function(){
 				hh = dt.getHours();
 				mm = dt.getMinutes();
 				ss = dt.getSeconds();
+
+				if(ss < 10 ){
+					ss = "0" + ss;
+				}
+
+				if( isOrange ){
+					$("body").css("background", "black");
+					$("body").css("color", "orange");
+					isOrange = false;
+				}else{
+					$("body").css("background", "orange");
+					$("body").css("color", "black");
+					isOrange = true;
+				}
 					// ss++ means ss = ss + 1
 					// ss++; 
 
